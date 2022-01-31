e23ea49469ae13da2781b8873da97a5bfe2b88f3
yozzzzy <95652054+yozzzzy@users.noreply.github.com>
2022-01-31 22:18:04 +0200

    hi

---
+++ src/test/bbb_test.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class bbb_test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","C:\\selenium\\browserDriver//chromedriver.exe");
+
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+
+	}
+
+}
